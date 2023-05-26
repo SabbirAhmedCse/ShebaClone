d543c93fc2c619d5f63e7061134ac5f7fddce1e5
asfaqur <asfaqurrashid@gmail.com>
2023-05-26 14:59:52 +0600

    Common Table Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,12 @@ import UpdateService from './Pages/UpdateService'
 import ServiceDetails from './Pages/ServiceDetails'
 import ServicesList from './Pages/ServicesList'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import CommonTable from "./Components/Table";
+import ServiceRoutes from "./Routes/ServiceRoutes";
 
 function App() {
-  
   return (
-    <Router>
-      <Routes>
-        <Route exact path='/' element={<ServicesList />}></Route>
-        <Route exact path='/Create' element={<CreateService />}></Route>
-        <Route exact path='/Update' element={<UpdateService />}></Route>
-        <Route exact path='/Details' element={<ServiceDetails />}></Route>
-        <Route exact path='/Services' element={<ServicesList />}></Route>
-      </Routes>
-    </Router>
+     <ServiceRoutes/>
   );
 }
 
--- client/src/Components/Table.jsx
+++ client/src/Components/Table.jsx
@@ -1,4 +1,40 @@
+import {Table, Button} from 'semantic-ui-react'
 
-export default function table(){
-    
+export default function CommonTable({data, headers, dbData}){
+
+
+    return (
+        <Table singleLine fixed>
+            <Table.Header>
+                <Table.Row>
+                    {
+                        headers.map((item) => {
+                            return(<Table.HeaderCell textAlign='center'>{item}</Table.HeaderCell>)                   
+                        })
+                    }
+                    <Table.HeaderCell colspan='3' textAlign='center'>Actions</Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
+                {
+                    data.map((item) => {
+                        return(
+                            <Table.Row>
+                                {
+                                    dbData.map((idx) => {
+                                        return (
+                                            <Table.Cell textAlign='center'>{item[idx]}</Table.Cell>
+                                        )
+                                    })
+                                }
+                                <div className='center'>
+                                    <Button type='submit'>Details</Button> <Button type='submit'>Update</Button> <Button type='submit'>Delete</Button>
+                                    </div>
+                            </Table.Row>
+                        )
+                    })
+                }
+            </Table.Body>
+        </Table>
+    );
 }
