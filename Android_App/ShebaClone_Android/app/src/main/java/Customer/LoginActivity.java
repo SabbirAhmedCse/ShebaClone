616f218bcb7af1f0f8d02c8d1d41b69e763d3000
asfaqur <asfaqurrashid@gmail.com>
2023-06-07 15:44:29 +0600

    Android_APp and Flutter_APP Added

---
+++ Android_App/ShebaClone_Android/app/src/main/java/Customer/LoginActivity.java
@@ -0,0 +1,16 @@
+package Customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shebaclone_android.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
