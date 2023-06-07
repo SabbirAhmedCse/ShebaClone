6e3a96604e37ee985d5f33f76741fead6cd0cdfd
asfaqur <asfaqurrashid@gmail.com>
2023-06-07 15:41:02 +0600

    Mechanic Changes

--- client/src/Pages/MechanicDetails.jsx
+++ client/src/Pages/MechanicDetails.jsx
@@ -1,107 +1,116 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
-import axios from 'axios'
-import { Table,Container,Header,  Button } from 'semantic-ui-react'
-
+import { Table, Button} from 'semantic-ui-react'
+import commonDataAccess from '../dataAccess/CommonDataAccess'
 
 const baseURL = 'https://localhost:7194/api/User?Id=';
-
 export default function MechanicDetails() {
-    const [post, setPost] = React.useState(null);
-   
+
+    const [data, setData] = useState([]);
+
     const nav = useNavigate();
     const location = useLocation();
-    const Uid = location.state;
+    const {link} = location.state;
 
-    React.useEffect(() => {
-        axios.get(baseURL+Uid).then((response) => {
-          setPost(response.data);
-        });
-      }, []);
-
-      if (!post) return null;
-     
+    useEffect(() => {
+        commonDataAccess.get(baseURL+ link.id).then((response) => {
+            setData(response);
+        }).catch((error) => {
+            console.error(error);
+        });  
+     }, []);
 
     return (
-      
-        
-        <div className='contain'>
-           
-            <Header as='h2' icon textAlign='center'>
-            <Header.Content>Mechanics Details</Header.Content>
-            </Header>
+        <div className="contain">
+            <div className="main">
+                <h2>Mechanic Details</h2>
+            </div>
             <Table striped>
                 <Table.Body>
                     <Table.Row>
-                        <Table.Cell>
-                            Mechanic ID :
+                        <Table.Cell width={10}>
+                            Mechanic ID
                         </Table.Cell>
-                        <Table.Cell>
-                            {post.id}
-                        </Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                        <Table.Cell>
-                           Name :
-                        </Table.Cell>
-                        <Table.Cell>
-                            {post.name}
+                        <Table.Cell textAlign='center'>
+                            {data.id}
                         </Table.Cell>
                     </Table.Row>
                     <Table.Row>
                         <Table.Cell>
-                          Mobile Number :
+                            Name
                         </Table.Cell>
-                        <Table.Cell>
-                            {post.mobileNumber}
-                        </Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                        <Table.Cell>
-                           Address :
-                        </Table.Cell>
-                        <Table.Cell>
-                            {post.address}
+                        <Table.Cell textAlign='center'>
+                            {data.name}
                         </Table.Cell>
                     </Table.Row>
-                    <Table.Row>
+                     <Table.Row>
                         <Table.Cell>
-                           Area :
+                           Email
                         </Table.Cell>
-                        <Table.Cell>
-                            {post.area}
-                        </Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                        <Table.Cell>
-                           City :
-                        </Table.Cell>
-                        <Table.Cell>
-                            {post.city}
+                        <Table.Cell textAlign='center'>
+                            {data.email}
                         </Table.Cell>
                     </Table.Row>
-                    <Table.Row>
+                     <Table.Row>
                         <Table.Cell>
-                          Expert :
+                          Mobile Number
                         </Table.Cell>
-                        <Table.Cell>
-                            {post.expert}
-                        </Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                        <Table.Cell>
-                         IsAvailable :
-                        </Table.Cell>
-                        <Table.Cell>
-                            {JSON.stringify(post.isActive)}
+                        <Table.Cell textAlign='center'>
+                            {data.mobileNumber}
                         </Table.Cell>
                     </Table.Row>
-                  
+                     <Table.Row>
+                        <Table.Cell>
+                          Gender
+                        </Table.Cell>
+                        <Table.Cell textAlign='center'>
+                            {data.gender}
+                        </Table.Cell>
+                    </Table.Row>
+                     <Table.Row>
+                        <Table.Cell>
+                          Date Of Birth
+                        </Table.Cell>
+                        <Table.Cell textAlign='center'>
+                            {data.dateOfBirth}
+                        </Table.Cell>
+                    </Table.Row>
+                     <Table.Row>
+                        <Table.Cell>
+                           City
+                        </Table.Cell>
+                        <Table.Cell textAlign='center'>
+                            {data.city}
+                        </Table.Cell>
+                    </Table.Row>
+                     <Table.Row>
+                        <Table.Cell>
+                           Area
+                        </Table.Cell>
+                        <Table.Cell textAlign='center'>
+                            {data.area}
+                        </Table.Cell>
+                    </Table.Row>
+                     <Table.Row>
+                        <Table.Cell>
+                          Address
+                        </Table.Cell>
+                        <Table.Cell textAlign='center'>
+                            {data.address}
+                        </Table.Cell>
+                    </Table.Row>
+                     <Table.Row>
+                        <Table.Cell>
+                          ISActive
+                        </Table.Cell>
+                        <Table.Cell textAlign='center'>
+                           {data.isActive ? "Active": "Inactive"}
+                        </Table.Cell>
+                    </Table.Row>
                 </Table.Body>
             </Table>
             <Button primary onClick={() => { nav('/Mechanics') }}>Back to Lists</Button>
-
-            </div>
+        </div>
     );
 }
