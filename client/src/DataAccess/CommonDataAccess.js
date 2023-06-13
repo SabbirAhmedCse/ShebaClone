42ce63abf89cc544aea6f71fde41e67f67accf88
SabbirAhmedCse <sabbir.cse.18@gmail.com>
2023-06-13 15:32:23 +0600

    client and api some modify

--- client/src/DataAccess/CommonDataAccess.js
+++ client/src/DataAccess/CommonDataAccess.js
@@ -4,30 +4,25 @@ import config from "../utils/config";
 const token = `Bearer ${config?.key?.token}`;
 
 const get = async (url) => {
-  try{
-    const list = (await axios.get(url, { headers: { Authorization: token } }));
-    console.log(list)
-    return  list.data;
-  }
-  catch(ex){
-
-  }
+  try {
+    const list = await axios.get(url, { headers: { Authorization: token } });
+    console.log(list);
+    return list.data;
+  } catch (ex) {}
 };
 const post = async (url, data) => {
   return (
     await axios.post(url, data, {
-      headers: { Authorization: config.key },
+      headers: { Authorization: token },
     })
   ).data;
 };
 const update = async (url, data) => {
-  return (
-    await axios.put(url, data, { headers: { Authorization: token } })
-  ).data;
+  return (await axios.put(url, data, { headers: { Authorization: token } }))
+    .data;
 };
 const remove = async (url) => {
-  return (await axios.delete(url, { headers: { Authorization: token } }))
-    .data;
+  return (await axios.delete(url, { headers: { Authorization: token } })).data;
 };
 const commonDataAccess = {
   get,
--- client/src/Pages/CustomerList.jsx
+++ client/src/Pages/CustomerList.jsx
@@ -1,79 +1,89 @@
-import { useState, useEffect } from 'react';
-import { Table, TableHeader, Pagination, Search, Button} from 'semantic-ui-react'
-import { useNavigate } from 'react-router-dom'
-import CommonTable from '../Components/Table';
-import commonDataAccess from '../dataAccess/CommonDataAccess';
+import { useState, useEffect } from "react";
+import config from "../utils/config";
+import {
+  Table,
+  TableHeader,
+  Pagination,
+  Search,
+  Button,
+} from "semantic-ui-react";
+import { useNavigate } from "react-router-dom";
+import CommonTable from "../Components/Table";
+import commonDataAccess from "../dataAccess/CommonDataAccess";
 
 function SearchBar() {
-    return (
-        <div className="main">
-            <div className="ui search">
-                <div className="ui icon input">
-                    <input className="prompt"
-                        type="text"
-                        placeholder="Search..." />
-                    <i className="search icon"></i>
-                </div>
-                <div className="results"></div>
-            </div>
+  return (
+    <div className="main">
+      <div className="ui search">
+        <div className="ui icon input">
+          <input className="prompt" type="text" placeholder="Search..." />
+          <i className="search icon"></i>
         </div>
-    );
+        <div className="results"></div>
+      </div>
+    </div>
+  );
 }
 
 export default function CustomerList() {
-    const [data, setData] = useState([]);
-    const nav = useNavigate();
-    var url = 'https://localhost:7194/api/User/userType?type=customer';
+  const [data, setData] = useState([]);
+  const nav = useNavigate();
+  var url = `${config.baseUrl}/User/userType?type=customer`;
 
-    const Columns = [
-        {
-            name: "Id",
-            value: "id"
-        },
-        {
-            name: "Name",
-            value: "name"
-        },
-        {
-            name: "Email",
-            value: "email"
-        },
-        {
-            name: "Mobile Number",
-            value: "mobileNumber"
-        },
-        {
-          name: "Area",
-          value: "area"
-      },
-      {
-        name: "Address",
-        value: "address"
+  const Columns = [
+    {
+      name: "Id",
+      value: "id",
     },
-    ]
-    const actions = ["Details"];
-
-    useEffect(() => {
-        commonDataAccess.get(url)
-          .then((data) => setData(data))
-          .catch((error) => {            
-            console.error('Error fetching data:', error);
-          });
-      }, [url]);
-
-    console.log(data);
-    const functions = [
-
-    (id)=> nav('/CustomerDetails', { state: { link: { url, id } } })
+    {
+      name: "Name",
+      value: "name",
+    },
+    {
+      name: "Email",
+      value: "email",
+    },
+    {
+      name: "Mobile Number",
+      value: "mobileNumber",
+    },
+    {
+      name: "Area",
+      value: "area",
+    },
+    {
+      name: "Address",
+      value: "address",
+    },
   ];
-    
-    return (
-        <div className='customerlist'>
-            <div className="main">
-                <h2>Customer List</h2>
-            </div>
-            <SearchBar />
-            <CommonTable data={data} Columns = {Columns} actions={actions} functions={functions}/>
-        </div>
-    );
+  const actions = ["Details"];
+
+  useEffect(() => {
+    commonDataAccess
+      .get(url)
+      .then((data) => setData(data))
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, [url]);
+
+  console.log(data);
+  const functions = [
+    (id) => nav("/CustomerDetails", { state: { link: { url, id } } }),
+  ];
+
+  return (
+    <div className="customerlist">
+      <div className="main">
+        <h2>Customer List</h2>
+      </div>
+      <SearchBar />
+      <CommonTable
+        data={data}
+        Columns={Columns}
+        actions={actions}
+        functions={functions}
+      />
+    </div>
+  );
 }
